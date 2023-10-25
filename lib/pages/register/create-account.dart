6abc883e1c3c5023a90a6cf58c3291eaaca22a3d
Osama Saleh <87075455+Elkapeer@users.noreply.github.com>
2023-10-26 00:00:00 +0300

    finished implementing create-account page UI

--- lib/pages/register/create-account.dart
+++ lib/pages/register/create-account.dart
@@ -1,9 +1,9 @@
-import 'dart:ffi';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gigachat/providers/theme-provider.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:intl/intl.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({Key? key}) : super(key: key);
@@ -13,32 +13,33 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  //input controllers
   TextEditingController inputName = TextEditingController();
   TextEditingController inputEmail = TextEditingController();
   TextEditingController inputDOB = TextEditingController();
+  //input validation
+  bool nameIsValid = false;
+  bool nameIsError = false;
+  bool emailIsValid = false;
+  bool emailIsError = false;
+  final formKey = GlobalKey<FormState>();
 
+  FocusNode dateFocusNode = FocusNode();  //to check for date text_field focus
 
   @override
   Widget build(BuildContext context) {
+
     ThemeProvider theme = ThemeProvider();
     bool isDark = theme.getThemeName == "dark";
-    Color backGroundColor = theme.getTheme.scaffoldBackgroundColor;
-    Color? appBarColor = theme.getTheme.appBarTheme.backgroundColor;
-    IconThemeData? appBarIconTheme = theme.getTheme.appBarTheme.iconTheme;
-    Color? textFieldBorderColor = theme.getTheme.textTheme.labelSmall?.color;
-    MaterialStateProperty<Color?>? buttonBackGroundColor = theme.getTheme.textButtonTheme.style?.backgroundColor;
-    MaterialStateProperty<TextStyle?>? buttonTextStyle = theme.getTheme.textButtonTheme.style?.textStyle;
-    Color? inputTextColor = theme.getTheme.textTheme.bodyMedium?.color;
+    bool isButtonDisabled = inputDOB.text.isEmpty || inputName.text.isEmpty || inputEmail.text.isEmpty;
 
     return Scaffold(
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
-        iconTheme: appBarIconTheme,
         toolbarHeight: 40,
         elevation: 0,
-        backgroundColor:appBarColor,
         title: Center(
-          child: Container(
+          child: SizedBox(
             height: 40,
             width: 40,
             child: Image.asset(
@@ -47,104 +48,170 @@ class _CreateAccountState extends State<CreateAccount> {
           ),
         ),
       ),
-      backgroundColor: backGroundColor,
       body: Padding(
-        padding: const EdgeInsets.all(24),
+        padding: const EdgeInsets.fromLTRB(0,0,0,50),
         child: SingleChildScrollView(
-          child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Create your account",
-              style: TextStyle(
-                color: theme.getTheme.textTheme.headlineLarge?.color,
-                fontSize: 30,
-                fontWeight: FontWeight.bold
-              ),
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Create your account",
+                  style: TextStyle(
+                      color: theme.getTheme.textTheme.headlineLarge?.color,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                Visibility(
+                  visible: MediaQuery.of(context).viewInsets.bottom == 0,  //visible when keyboard is not shown
+                    child: const SizedBox(height: 130,)
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0,10,0,50),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: inputName,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (String? input){
+                            if(input == null || input.isEmpty){
+                              nameIsValid = false;
+                              nameIsError = false;
+                              return null;
+                            }
+                            else if(input.length > 50){
+                              nameIsValid = false;
+                              nameIsError = true;
+                              return "Must be 50 characters or fewer";
+                            }else{
+                              nameIsValid = true;
+                              nameIsError = false;
+                              return null;
+                            }
+                          },
+                          onChanged: (String input) async {
+                            await Future.delayed(const Duration(milliseconds: 50));  //wait for validator
+                            setState(() {});
+                          },
+                          decoration: InputDecoration(
+                            labelText: "Name",
+                            counterText: "${inputName.text.length}/50",
+                            border: const OutlineInputBorder(),
+                            enabledBorder: const OutlineInputBorder(),
+                            suffixIcon: nameIsValid?
+                            const Icon(Icons.check_circle_sharp,color: CupertinoColors.systemGreen,) :
+                            nameIsError? const Icon(Icons.error,color: Colors.red,) : null,
+                          ),
+                        ),
+                        const SizedBox(height: 24,),
+                        TextFormField(
+                          controller: inputEmail,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (String? input){
+                            if(input == null || input.isEmpty){
+                              emailIsValid = false;
+                              emailIsError = false;
+                              return null;
+                            }
+                            if(EmailValidator.validate(input)){
+                              emailIsError = false;
+                              emailIsValid = true;
+                              return null;
+                            }
+                            else{
+                              emailIsError = true;
+                              emailIsValid = false;
+                              return "Please enter a valid email";
+                            }
+                          },
+                          onChanged: (String input) async {
+                            await Future.delayed(const Duration(milliseconds: 50));  //wait for validator
+                            setState(() {});
+                          },
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            border: const OutlineInputBorder(),
+                            enabledBorder: const OutlineInputBorder(),
+                            suffixIcon: emailIsValid? const Icon(Icons.check_circle_sharp,color: CupertinoColors.systemGreen,) :
+                            emailIsError? const Icon(Icons.error,color: Colors.red,) : null,
+                          ),
+                        ),
+                        const SizedBox(height: 24,),
+                        TextFormField(
+                          controller: inputDOB,
+                          readOnly: true,
+                          focusNode: dateFocusNode,
+                          onTap: (){
+                            setState(() {});
+                          },
+                          decoration: const InputDecoration(
+                            labelText: "Date of birth",
+                            border: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
             ),
-            const SizedBox(height: 100,),
-            SizedBox(
-              height: 275,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextFormField(
-                    style: TextStyle(
-                      color: inputTextColor,
-                    ),
-                    decoration: InputDecoration(
-                      labelText: "Name",
-                      border: const OutlineInputBorder(),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: textFieldBorderColor!)
-                      ),
-
-                    ),
-                  ),
-                  TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (String? input){
-                      print(inputEmail.text);
-                      if(input != null && !EmailValidator.validate(input)){
-                        return "Please enter a valid email";
-                      }else {
-                        return null;
-                      }
-                    },
-                    style: TextStyle(
-                      color: inputTextColor,
-                    ),
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      border: const OutlineInputBorder(),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: textFieldBorderColor)
-                      ),
-                    ),
-                  ),
-                  TextFormField(
-                    style: TextStyle(
-                      color: inputTextColor,
-                    ),
-                    decoration: InputDecoration(
-                      labelText: "Date of birth",
-                      border: const OutlineInputBorder(),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: textFieldBorderColor)
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
           ),
         ),
       ),
-      bottomSheet: Container(
-        height: 64,
+      bottomSheet: SizedBox(
+        height: dateFocusNode.hasFocus? 150 : 50,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Divider(thickness: 0.2, color: textFieldBorderColor,),
+            const Divider(thickness: 0.2, height: 1,),
             Padding(
               padding: const EdgeInsets.fromLTRB(0,0,10,0),
               child: TextButton(
-                  onPressed: (){},
+
+                onPressed: isButtonDisabled? null : (){
+                  if(formKey.currentState!.validate()){
+                    //TODO: back-end post request
+                    //TODO: Navigate to other pages
+                    showDialog(context: context,
+                        builder: (context) => const Text("Noice")
+                    );
+                  }
+                },
                 style: ButtonStyle(
                   splashFactory: NoSplash.splashFactory,
                   overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  backgroundColor: buttonBackGroundColor,
-                  textStyle: buttonTextStyle,
                   shape: MaterialStateProperty.all(
                       const RoundedRectangleBorder(
                         borderRadius: BorderRadius.all(Radius.circular(20)),
                       )
                   ),
                 ),
-                  child: const Text("Next"),
+                child: const Text("Next"),
               ),
-            )
+            ),
+            Visibility(
+              visible: dateFocusNode.hasFocus,  //visible when date text_field is focused
+              child: SizedBox(
+                height: 100,
+                child: CupertinoDatePicker(
+                  mode: CupertinoDatePickerMode.date,
+                  initialDateTime: DateTime.now(),
+                  maximumDate: DateTime.now(),
+                  onDateTimeChanged: (input){
+                    setState(() {
+                      inputDOB.text = DateFormat.yMMMMd('en_US').format(input);
+                    });
+                  },
+                ),
+              )
+            ),
           ],
         ),
       ),
