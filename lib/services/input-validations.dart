7faad08a01d0512a4383d84d2dcde398f3e6e07f
xAbdoMo <82497296+AbdoWise-z@users.noreply.github.com>
2023-11-04 11:54:19 +0200

    Revert "handling navigation bugs and password validations"

--- lib/services/input-validations.dart
+++ lib/services/input-validations.dart
@@ -5,37 +5,21 @@ import 'package:string_validator/string_validator.dart';
 const MIN_USERNAME_LENGTH = 6;
 const MAX_USERNAME_LENGTH = 20;
 
-const MIN_PASSWORD_LENGTH = 6;
-const MAX_PASSWORD_LENGTH = 64;
-
-
-
 class InputValidations
 {
-     static String _buildShortFieldError(String fieldName, int min) => "$fieldName must be at least $min characters";
-     static String _buildLongFieldError(String fieldName, int max) => "$fieldName must be at most $max characters";
-
-     static String emptyErrorMessage = "this field cannot be empty";
-     static String passwordShortInputError = _buildShortFieldError("password", MIN_PASSWORD_LENGTH);
-     static String passwordLongInputError = _buildLongFieldError("password", MAX_PASSWORD_LENGTH);
-     static String passwordInvalidInput = "invalid input, only characters and numbers and some special characters are allowed";
-     static String passwordWeakError = "weak password";
-
-     static String usernameShortInputError = _buildShortFieldError("password", MIN_USERNAME_LENGTH);
-     static String usernameLongInputError = _buildLongFieldError("password", MAX_USERNAME_LENGTH);
 
      static String? verifyUsername(String? username)
      {
           if (username == null || username.isEmpty) {
-            return emptyErrorMessage;
+            return "empty username!";
           }
 
           if (username.length < MIN_USERNAME_LENGTH) {
-            return usernameShortInputError;
+            return "username must be at least $MIN_USERNAME_LENGTH characters.";
           }
 
           if (username.length > MAX_USERNAME_LENGTH) {
-            return usernameLongInputError;
+            return "username must be at most $MAX_USERNAME_LENGTH characters.";
           }
 
           return null;
@@ -43,17 +27,14 @@ class InputValidations
 
      static String? verifyPassword(String? password)
      {
-          if (password == null || password.isEmpty) return emptyErrorMessage;
-          if (password.length < MIN_PASSWORD_LENGTH) return passwordShortInputError;
-          if (password.length > MAX_PASSWORD_LENGTH) return passwordLongInputError;
-
-          RegExp validCharacterRegex = RegExp("[a-zA-Z0-9@#_!\$]");
-          if(validCharacterRegex.allMatches(password).length != password.length){
-               return passwordInvalidInput;
+          if (password == null || password.isEmpty) {
+            return "password cannot be empty.";
           }
 
-
+          RegExp regex = RegExp("[a-zA-Z0-9@#_\$]");
           RegExp specialChars = RegExp("[@!\$#_-]");
+          bool acceptedLength = password.length >= 10 && password.length <= 64;
+          bool satisfyRegex = regex.hasMatch(password);
           bool hasLowerCase = false;
           bool hasUpperCase = false;
           bool hasNumber = false;
@@ -67,7 +48,14 @@ class InputValidations
                hasSpecial |= specialChars.hasMatch(password[i]);
           }
 
-          return hasSpecial && hasLowerCase && hasUpperCase && hasNumber ? null : passwordWeakError;
+          if(acceptedLength && satisfyRegex && hasSpecial && hasLowerCase && hasUpperCase && hasNumber)
+          {
+               return null;
+          }
+          else
+          {
+               return "invalid password";
+          }
      }
 
      static String? isValidEmail(String? email)
