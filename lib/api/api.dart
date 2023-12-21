bad34c947d8b7e1b3d1f3635212dc2f8dd8e121e
AbdoWise-z <moh2002.abdow@gmail.com>
2023-12-22 01:37:39 +0200

    added some api requests

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -115,6 +115,7 @@ class ApiPath{
   static ApiPath chatMessages            = const ApiPath._("/api/user/chat/%s");
   static ApiPath getTweet                = const ApiPath._("/api/tweets/%s");
   static ApiPath chatSearch              = const ApiPath._("/api/user/chat/search");
+  static ApiPath searchTrends            = const ApiPath._("/api/trends/%s");
 }
 
 class Api {
